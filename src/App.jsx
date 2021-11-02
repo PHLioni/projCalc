928c0ce89b041fe0b132190a67cf2e701828dc67
PHLioni <ph.lioni@hotmail.com>
2021-11-01 22:00:08 -0300

    first

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+import Calculadora from "./components/calculadora/Calculadora";
+
+export default () => {
+  return (
+    <div className="container--main">
+      <Calculadora />
+    </div>
+  );
+};
---
+++ src/components/calculadora/Calculadora.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Calculadora.css";
+
+export default () => {
+  return (
+    <div>
+      <h1 className="container--title">Calculadora</h1>
+      <div className="container--calc"></div>
+    </div>
+  );
+};
